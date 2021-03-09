50ff65aee6323904da30245afc26f3e35ec26105
Brad Moore <bmoore6237@gmail.com>
2021-03-09 11:47:28 -0600

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,36 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// allow requests from localhost:4200 (cors error)
+@CrossOrigin(origins="http://localhost:4200")
+public class HelloWorldController {
+	
+	// GET
+	// URI - /hello-world
+	// method - "Hello World"
+	
+	// @GetMapping(path="/hello-world") works too
+	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("HelloWorld");
+	}
+	
+	// hello-world/path-variable/in28minutes
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		//throw new RuntimeException("Something went wrong");
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
